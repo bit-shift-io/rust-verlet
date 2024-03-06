ed35ea380b1ce23d66004d104d5516abe101dae1
Fabian Mathews <fabian@jpmedia.com.au>
2024-03-06 17:39:48 +1030

    WIP car scene

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod particle;
 mod stick;
 mod cloth;
 mod scenes {
+    pub mod car_scene;
     pub mod cloth_scene;
     pub mod random_bodies_scene;
 }
@@ -12,11 +13,13 @@ mod scenes {
 use crate::application::Application;
 use crate::sdl_system::SdlSystem;
 use crate::scenes::random_bodies_scene::RandomBodiesScene;
+use scenes::car_scene::CarScene;
 use scenes::cloth_scene::ClothScene;
 
 fn main() -> Result<(), String> {
     let mut sdl = SdlSystem::new("Rust Verlet", 1200, 800);
     let mut application = Application::new(&mut sdl);
+    application.register_scene(Box::new(CarScene::new()));
     application.register_scene(Box::new(RandomBodiesScene::new()));
     application.register_scene(Box::new(ClothScene::new()));
     return application.run();
