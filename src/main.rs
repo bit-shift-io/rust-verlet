6233424a3b323ac92d66e206c323be58a791b3e6
Fabian Mathews <fabian@jpmedia.com.au>
2024-06-30 16:53:22 +0930

    bevy stuff

--- src/main.rs
+++ src/main.rs
@@ -56,11 +56,23 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>
+) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
+
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
     for mut name in &mut query {
@@ -71,14 +83,21 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
+
+// Bevy next steps: https://bevyengine.org/learn/quick-start/next-steps/
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world_system, (update_people, greet_people).chain()))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
-}
-
-fn hello_world_system() {
-    println!("hello world");
 }
