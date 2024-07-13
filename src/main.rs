63bb634b2373d05e3d0e20dd46ade3dd95a5fc66
Fabian Mathews <fabian@jpmedia.com.au>
2024-07-13 15:30:34 +0930

    got mesh instancing exmaple working in bevy 0.14

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ mod mouse;
 mod application;
 mod point;
 
-mod main_bevy;
+//mod main_bevy;
 mod main_sdl;
+mod shader_instancing;
 
 mod scenes {
     pub mod car {
@@ -29,11 +30,14 @@ mod v3 {
 }
 
 use crate::main_sdl::main_sdl;
-use crate::main_bevy::main_bevy;
+//use crate::main_bevy::main_bevy;
+use crate::shader_instancing::b_main;
 
 fn main() -> Result<(), String> {
     // this lets us switch between the old SDL renderer and the new Bevy renderer
     //return main_sdl();
-    return main_bevy();
+    //return main_bevy();
+    b_main();
+    Ok({})
 }
 
