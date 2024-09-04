d5ab8165aa94c9ca6080085fa4135fd1a60ab1d5
Fabian Mathews <fabian@jpmedia.com.au>
2024-09-04 15:42:38 +0930

    added comment

--- src/v4/constraints/constraint.rs
+++ src/v4/constraints/constraint.rs
@@ -2,5 +2,7 @@
 
 
 pub trait Constraint {
+    /// https://users.rust-lang.org/t/solved-is-it-possible-to-clone-a-boxed-trait-object/1714/7
+    /// todo: look at the iproved version
     fn box_clone(&self) -> Box<dyn Constraint>;
 }
